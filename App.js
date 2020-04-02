474c24f28b83a3e08c6868b8b338430502f346d5
Sangwook Lee <leesang627@gmail.com>
2020-04-02 23:37:42 +0900

    UI Basic Layout

--- App.js
+++ App.js
@@ -10,70 +10,147 @@ import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
+  Image,
+  FlatList,
+  TextInput,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const commentDummy = [
+  {
+    id: 1,
+    userName: 'YANGWOO LEE',
+    userImage: 'http://www.meconomynews.com/news/photo/201911/35379_37255_3949.png',
+    uploadTime: '21시간 전',
+    time: '2:33',
+    comment: '데톨은 옥시에서 만든겁니다... 가습기 살균제 사건 잊으셨나요?',
+    like: 105,
+  },
+  {
+    id: 2,
+    userName: 'gentle guy',
+    userImage: 'https://i.insider.com/5ba15375e361c01c008b5cf7?width=2500&format=jpeg&auto=webp',
+    uploadTime: '21시간 전',
+    time: '1:30',
+    comment: '코로나.. 이시국 최대의 피해자...',
+    like: 33,
+  },
+]
+
+const ListHeader = () => {
+  return (
+    <>
+      <View style={styles.infoContainer}>
+        <Text style={styles.videoTitle}>
+          Video Title
+        </Text>
+        <Text style={styles.videoUploaderName}>
+          Video Uploader
+        </Text>
+      </View>
+      <View style={styles.commentsHeaderContainer}>
+        <Text style={styles.commentsText}>댓글 141</Text>
+        <View style={styles.commentInputContainer}>
+          <Image
+            style={styles.profileImage}
+            source={{
+              uri: 'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
+            }}
+          />
+          <Text style={styles.timeInput}>2:12</Text>
+          <TextInput 
+            style={styles.textInput}
+            placeholder='키워드 검색...'
+          />
+        </View>
+      </View>
+    </>
+  )
+}
+
+const CommentItem = ({data}) => {
+  return (
+    <View>
+      <Text>{data.comment}</Text>
+    </View>
+  )
+}
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View>
-          <Text>Hello, World!</Text>
+      <SafeAreaView style={styles.appContainer}>
+        <View style={styles.playerContatiner}>
+
         </View>
+        <FlatList 
+          style={styles.systemList}
+          ListHeaderComponent={ListHeader}
+          data={commentDummy}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <CommentItem data={item}/>}
+        />
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  appContainer: {
+    flex:1,
+    flexDirection: 'column',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  playerContatiner: {
+    backgroundColor: 'black',
+    aspectRatio: 16/9,
   },
-  body: {
-    backgroundColor: Colors.white,
+  systemList: {
+    backgroundColor: 'grey',
+    flex: 1,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  infoContainer: {
+    backgroundColor: 'red',
+    aspectRatio: 4,
+    padding: 15,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
+  videoTitle: {
     fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+    marginBottom: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  videoUploaderName: {
+    fontSize: 20,
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  commentsHeaderContainer: {
+    padding: 15,
+    backgroundColor: 'white',
+    height: 100,
   },
+  commentsText: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  commentInputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  profileImage:{
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    marginRight: 15,
+  },
+  timeInput: {
+    fontSize: 15,
+    color: '#1366D4',
+    marginRight: 5,
+  },
+  textInput: {
+    fontSize: 15,
+    color: '#606060',
+  }
 });
 
 export default App;
